3e73708e3e87c41f6e05a53684a51f601f5c2469
MartyVos <marty.vos@student.hu.nl>
2018-11-22 14:31:19 +0100

    Added Doxygen Documentation

--- lib/IKeyboardListener.hpp
+++ lib/IKeyboardListener.hpp
@@ -3,6 +3,8 @@
 
 class IKeyboardListener{
 public:
+	///@fn virtual void IKeyboardListener::KeyboardKeyPressed(char Key)=0
+    ///@param ButtonID A key from the keyboard.
 	virtual void KeyboardKeyPressed(char Key) = 0;
 };
 
